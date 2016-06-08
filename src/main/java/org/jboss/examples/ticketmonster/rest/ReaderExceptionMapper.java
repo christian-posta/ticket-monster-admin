a002d9b75f5d41b9fc2e6260a3f65f3a02b05cc0
Christian Posta <christian.posta@gmail.com>
2016-06-08 14:20:58 -0700

    convert to wildfly-swarm!

--- src/main/java/org/jboss/examples/ticketmonster/rest/ReaderExceptionMapper.java
+++ src/main/java/org/jboss/examples/ticketmonster/rest/ReaderExceptionMapper.java
@@ -1,7 +1,5 @@
 package org.jboss.examples.ticketmonster.rest;
 
-import org.jboss.resteasy.spi.ReaderException;
-
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
@@ -18,14 +16,14 @@ import java.util.Map;
  * By default, a text/html response is generated that would violate HTTP semantics.
  */
 @Provider
-public class ReaderExceptionMapper implements ExceptionMapper<ReaderException> {
+public class ReaderExceptionMapper implements ExceptionMapper<Exception> {
 
     @Context
     private HttpHeaders headers;
 
 
     @Override
-    public Response toResponse(ReaderException exception) {
+    public Response toResponse(Exception exception) {
         Map<String, Object> responseEntity = new HashMap<String, Object>();
         responseEntity.put("errors", Collections.singletonList("The submitted request is invalid. Please retry after correcting the input values."));
         return Response.status(Response.Status.BAD_REQUEST).
